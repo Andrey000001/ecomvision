e7679541e086972dd49badeb1e0bf13250b75b49
Andrii <kovratovskiy1999@gmail.com>
2025-05-18 00:03:34 -0400

    Modify server set up mongo db and mongoose and express

---
+++ server/index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import clientRoutes from './routes/client.js';
+import generalRoutes from './routes/general.js';
+import managementRoutes from './routes/management.js';
+import salesRoutes from './routes/sales.js';
+
+/*CONFIGURATION*/
+dotenv.config()
+const app = express();
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({police: "cross-origin"}))
+app.use(morgan("common"))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cors())
+
+/*ROUTES*/
+app.use('/client',clientRoutes)
+app.use('/general',generalRoutes)
+app.use('/managment',managementRoutes)
+app.use('/sales',salesRoutes)
+
+
+/*MONGOOSE SETUP*/
+const PORT = process.env.PORT || 9000
+mongoose.connect(process.env.MONGO_URL).then((req,res) => {
+    app.listen(PORT,() => {
+        console.log(`Server is running on port ${PORT}`)
+    })
+}).catch((error) => {
+    console.log(`${error} did not connect`)
+})
