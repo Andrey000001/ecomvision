bab4c4bb775af08fc18e337d856888d83b9858b9
Andrii <kovratovskiy1999@gmail.com>
2025-05-18 23:12:52 -0400

    creat components scenes state set up index and app jsconfig .prettierrc

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  useTheme,
+} from '@mui/material';
+import {
+  SettingsOutlined,
+  ChevronLeft,
+  ChevronRightOutlined,
+  HomeOutlined,
+  ShoppingCartOutlined,
+  Groups2Outlined,
+  ReceiptLongOutlined,
+  PublicOffOutlined,
+  PointOfSaleOutlined,
+  TodayOutlined,
+  CalendarMonthOutlined,
+  AdminPanelSettingsOutlined,
+  TrendingUpOutlined,
+  PieChartOutline,
+} from '@mui/icons-material';
+const Sidebar = ({ isSidebarOpen, setIsSidebarOpen }) => {
+  return <div>Sidebar</div>;
+};
+
+export default Sidebar;
